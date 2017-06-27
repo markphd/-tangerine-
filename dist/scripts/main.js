fbc8da6b2c839c9f8e5abb0afa708ca8b7f4c4fb
markphd <radical.mark@gmail.com>
2017-06-27 17:37:44 +0400

    add dist folder

---
+++ dist/scripts/main.js
@@ -0,0 +1,2 @@
+main.js;
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsibWFpbiIsImpzIl0sIm1hcHBpbmdzIjoiQUFBQUEsS0FBS0MiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1haW4uanMiXX0=
