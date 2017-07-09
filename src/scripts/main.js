5d0c1ab7b4450a83c75c482036b8c386e24c2ddc
markphd <radical.mark@gmail.com>
2017-07-09 16:17:47 +0400

    mobile view progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-main.js
+var elem = $("#password");
+
+elem.focus(function() {
+	$("p.inner--label").addClass('hide');
+});
+
+elem.focusout(function() {
+	if (elem.val() === "") {
+		$("p.inner--label").removeClass('hide');
+	}
+})
