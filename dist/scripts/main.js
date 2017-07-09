59f28f4e1a8396b7041c2f8d9426cdbb4fc5ae8b
markphd <radical.mark@gmail.com>
2017-07-09 13:11:00 +0400

    start mobile view

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiIl19
-=======
 main.js;
 //# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsibWFpbiIsImpzIl0sIm1hcHBpbmdzIjoiQUFBQUEsS0FBS0MiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1haW4uanMiXX0=
->>>>>>> 7929022d371394982619081c156c1e571c7e008d
