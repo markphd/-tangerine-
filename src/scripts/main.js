6293fdf954f35a1b1e107aad33e3795d4282e48b
Mark Philip Dawal <radical.mark@gmail.com>
2017-07-15 20:00:22 +0400

    implementation of show/hide password

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,3 +20,41 @@ var resetButton = $(".form--button");
 resetButton.click(function() {
 	console.log("Clicked");
 })
+
+
+// 3. Show/Hide Confirm password
+$('#new-password').hidePassword({
+	innerToggle: true,
+	enforceWidth: false,
+	toggle: {
+		element: '<p>',
+		className: 'inner--label',
+		styles: { 'margin': '0 2% 0 0', 'padding': '1%', 'display': 'inline' }
+	},
+	wrapper: {
+		enforceWidth: false,
+		styles: { 'margin': '0', 'padding': '0' , 'display': 'inline'}
+	}
+});
+
+$('#new-password').on('focus', function() {
+	$('label[for="new-password"]').addClass('active');
+});
+
+$('#confirm-password').hidePassword({
+	innerToggle: true,
+	enforceWidth: false,
+	toggle: {
+		element: '<p>',
+		className: 'inner--label',
+		styles: { 'margin': '0 2% 0 0', 'padding': '1%', 'display': 'inline' }
+	},
+	wrapper: {
+		enforceWidth: false,
+		styles: { 'margin': '0', 'padding': '0' , 'display': 'inline'}
+	}
+});
+
+$('#confirm-password').on('focus', function() {
+	$('label[for="confirm-password"]').addClass('active');
+})
