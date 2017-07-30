52b406df784220a7a15911a69f147e823619eefe
Mark Philip Dawal <radical.mark@gmail.com>
2017-07-31 00:48:11 +0400

    BUG: input form performance issue and behaviour

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,4 @@
 $( document ).ready(function() {
-	// 1. Password field helper
-
-	var elem = $("#password");
-
-	elem.focus(function() {
-		$("p.inner--label").addClass('hide');
-	});
-
-	elem.focusout(function() {
-		if (elem.val() === "") {
-			$("p.inner--label").removeClass('hide');
-		}
-	})
-
-
-	// 2. Forgot password button pre-loader
-
-	var resetButton = $(".form--button");
-
-	resetButton.click(function() {
-		console.log("Clicked");
-	})
-
 
 	// 3. Show/Hide Confirm password
 	$('#new-password').hidePassword({
